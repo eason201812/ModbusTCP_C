5566028ee76740f22a0c171c4e18f9f2cd976a5b
Eason201812 <joubonabox@gmail.com>
2022-02-27 10:36:29 +0800

    Complete Holding Register cmd

---
+++ mod_profile.h
@@ -0,0 +1,3 @@
+#define IP "192.168.11.153"
+#define SLAVE_ID 1
+#define PORT 502
---
+++ write_register.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <modbus.h>
+#include <errno.h>
+#include "mod_profile.h"
+
+int main(int argc,char *argv[]){
+	int addr = atoi(argv[1]);
+	int value = atoi(argv[2]);
+	printf("Write to addr:%xH,value = %d\n",addr,value);
+
+	// 1.Define Modbus
+	modbus_t *mb;
+	mb = modbus_new_tcp(IP,PORT);
+	modbus_set_slave(mb,SLAVE_ID);
+	modbus_connect(mb);	
+
+	// 2.Try to connect
+	if(mb == NULL){
+		fprintf(stderr,"Unable to allocate limodbus context\n");
+		return -1;
+	}
+
+	if(modbus_connect(mb) == -1){
+		fprintf(stderr,"Connection failed:%s\n",modbus_strerror(errno));
+		modbus_free(mb);
+		return -1;
+	}
+
+	// 3.Write to register
+	int result = 0;
+	result = modbus_write_register(mb,addr,value);
+	printf("result: %d\n",result);	
+
+	// 4.close
+	modbus_close(mb);
+	modbus_free(mb);	
+}
